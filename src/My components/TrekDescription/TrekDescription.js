2f3b31aa774f33322ab3e07f13130c31ee3a9e2a
Kaustub <kaustubniraula999@gmail.com>
2022-07-29 18:22:20 +0545

    trek_destination_details added

--- src/My components/TrekDescription/TrekDescription.js
+++ src/My components/TrekDescription/TrekDescription.js
@@ -1,60 +1,17 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import "./TrekDescription.css"
+import {desc} from "./trek_destination_details"
 
 export const TrekDescription = () => {
   const params = useParams();
-
-  let desc = {
-      title: "Annapurna Base Camp",
-      description: "Annapurna Base Camp Trek probably the best legendary and classical treks in the world offers breathe stopping Mountain View, give opportunity to experience the typical Nepali village, local people and their way of living traditional life in Himalaya of Nepal. The Annapurna Base Camp route goes passing through spectacular and tranquil landscapes, charming Gurung and Magar villages, lush green Rhododendron, bamboo and alpine forests to the trip’s last and final destination, Annapurna Base Camp at the height of 4130m (13546ft.)",
-      pic: "https://excitingnepal.com/wp-content/uploads/2019/06/annapurna-base-camp-trek-map.jpg",
-
-      route:[
-        {
-          city:[
-            {
-              city_name:"",
-              City_viewpoint:"",
-              accomodation:"" 
-            },
-            {
-              city_name:"",
-              City_viewpoint:"",
-              accomodation:"" 
-            }
-          ],
-        },
-        {
-          city:[
-            {
-              city_name:"",
-              City_viewpoint:"",
-              accomodation:"" 
-            },
-            {
-              city_name:"",
-              City_viewpoint:"",
-              accomodation:"" 
-            }
-          ],
-        }
-      ]
-
-  };
-
-  
-    let cities = [
-      "ghandruk", "chhomrong" , "sinuwa" , "bamboo" , "dovan" , "Himalaya" , "Deurali" , "Machhapuchhure Base Camp" , 
-      "ABC"
-  ];
 
   return (
     <>
     <div>
       <img src={desc.pic} alt="" />
 
-      <p className='text_area'>{params.title}</p> 
+      <p className='text_area'>{desc.description}</p> 
     </div>
     </>
   )
---
+++ src/My components/TrekDescription/trek_destination_details.js
@@ -0,0 +1,109 @@
+export const desc = {
+    title: "Annapurna Base Camp",
+    description: "Annapurna Base Camp Trek probably the best legendary and classical treks in the world offers breathe stopping Mountain View, give opportunity to experience the typical Nepali village, local people and their way of living traditional life in Himalaya of Nepal. The Annapurna Base Camp route goes passing through spectacular and tranquil landscapes, charming Gurung and Magar villages, lush green Rhododendron, bamboo and alpine forests to the trip’s last and final destination, Annapurna Base Camp at the height of 4130m (13546ft.)",
+    pic: "https://excitingnepal.com/wp-content/uploads/2019/06/annapurna-base-camp-trek-map.jpg",
+
+    route:[
+      {
+        city:[
+          {
+            city_name:"Ghandruk",
+            City_viewpoint:[
+              {
+                cvp_name:"",
+                cvp_details:""
+              },
+              {
+                cvp_name:"",
+                cvp_details:""
+              }
+            ],
+            accomodation:[
+              {
+                hotel_name:"",
+                phone_number:"",
+                email:""
+              },
+              {
+                hotel_name:"",
+                phone_number:"",
+                email:""
+              }
+            ]
+          },
+          {
+            city_name:"Chhomrong",
+            City_viewpoint:[
+              {
+                cvp_name:"",
+                cvp_details:""
+              },
+              {
+                cvp_name:"",
+                cvp_details:""
+              }
+            ],
+            accomodation:[
+              {
+                hotel_name:"",
+                phone_number:"",
+                email:""
+              },
+              {
+                hotel_name:"",
+                phone_number:"",
+                email:""
+              }
+            ]
+          }
+        ],
+      },
+    ],
+    season:["September","October"],
+    Guides:[
+      {
+        guide_name:"aashish pant",
+        packages:[
+          {
+            package_name: "",
+            package_cost:"",
+            package_details:""
+          },
+          {
+            package_name: "",
+            package_cost:"",
+            package_details:""
+          },
+        ]
+      },
+      {
+        guide_name:"paras pujara",
+        packages:[
+        {
+          package_name: "",
+          package_cost:"",
+          package_details:""
+        },
+        {
+          package_name: "",
+          package_cost:"",
+          package_details:""
+        },
+      ]
+      }
+    ],
+    transport:[
+      {
+        transport_medium:"car",
+        destination_city:"Ghandruk",
+        origin_city:["kathmandu","pokhara"],
+        cost:["10000","1000"]
+      },
+      {
+        transport_medium:"bus",
+        destination_city:"Ghandruk",
+        origin_city:["kathmandu","pokhara"],
+        cost:["10000","1000"]
+      }
+    ]
+};
